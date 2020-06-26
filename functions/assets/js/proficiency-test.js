939efb9b07f626e27f95dc366d85333e449d37ff
gabriel <akingdebased@gmail.com>
2020-06-26 03:00:14 -0700

    add error handling, attempt to change email settings

--- functions/assets/js/proficiency-test.js
+++ functions/assets/js/proficiency-test.js
@@ -1,168 +1,174 @@
 const mainContentEl = document.querySelector('.pt__main-content'),
-    preArrivalTestTemplate = document.querySelector('#pre-arrival-test-template').content,
-    preArrivalTestNode = document.importNode(preArrivalTestTemplate, true),
-    proficiencyTestTemplate = document.querySelector('#proficiency-test-template').content,
-    proficiencyTestNode = document.importNode(proficiencyTestTemplate, true),
-    completedTemplate = document.querySelector('#completed-template').content,
-    completedNode = document.importNode(completedTemplate, true);
+	preArrivalTestTemplate = document.querySelector('#pre-arrival-test-template').content,
+	preArrivalTestNode = document.importNode(preArrivalTestTemplate, true),
+	proficiencyTestTemplate = document.querySelector('#proficiency-test-template').content,
+	proficiencyTestNode = document.importNode(proficiencyTestTemplate, true),
+	completedTemplate = document.querySelector('#completed-template').content,
+	completedNode = document.importNode(completedTemplate, true);
 
 // Always render the pre-arrival-test-template on page load
 mainContentEl.appendChild(preArrivalTestNode);
 
 // Catch all handler for delegated click events
 mainContentEl.addEventListener('click', e => {
-    const clickedEl = e.target;
+	const clickedEl = e.target;
 
-    if (clickedEl.classList.contains('pt__begin-test-button')) {
-        const postPreArrivalTest = async (userInfo) => {
-            const response = await fetch('/pre-arrival-test', { 
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(userInfo)
-            });
+	if (clickedEl.getAttribute('type') === 'radio') {
+		console.log('yeet');
+		const errorContainer = document.querySelector('.pt__error-container');
 
-            return response;
-        }
-    
-        const preTestEls = Array.apply(null, document.querySelectorAll('.pt__pretest-item'));
+		if (errorContainer) errorContainer.classList.add('is-hidden'); 
+	}
 
-        // TODO: Ensure these inputs are VALIDATED and SANITIZED. This is unknown text input from a user, this is a prime XSS vector.
-        // Remember, validate.js is a thing
-        const userInfo = preTestEls.reduce((accum, preTestEl) => {
-            switch (preTestEl.nodeName) {
-                // TODO: Kludgy stop gap for the agency field. Needs to be more robust, and be tied to whether or not the user seleted the agency radio
-                case 'INPUT': {
-                    let inputValue = preTestEl.value.trim();
+	if (clickedEl.classList.contains('pt__begin-test-button')) {
+		const postPreArrivalTest = async (userInfo) => {
+			const response = await fetch('/pre-arrival-test', { 
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(userInfo)
+			});
 
-                    if (preTestEl.name === 'agencyName' && !inputValue) inputValue = 'No agency provided';
+			return response;
+		}
+	
+		const preTestEls = Array.apply(null, document.querySelectorAll('.pt__pretest-item'));
 
-                    accum[preTestEl.name] = inputValue;
-                    return accum;
-                }
+		// TODO: Ensure these inputs are VALIDATED and SANITIZED. This is unknown text input from a user, this is a prime XSS vector.
+		// Remember, validate.js is a thing
+		const userInfo = preTestEls.reduce((accum, preTestEl) => {
+			switch (preTestEl.nodeName) {
+				// TODO: Kludgy stop gap for the agency field. Needs to be more robust, and be tied to whether or not the user seleted the agency radio
+				case 'INPUT': {
+					let inputValue = preTestEl.value.trim();
 
-                case 'SELECT': {
-                    const formattedOption = preTestEl.options[preTestEl.selectedIndex].text.trim().toLowerCase();
+					if (preTestEl.name === 'agencyName' && !inputValue) inputValue = 'No agency provided';
 
-                    accum[preTestEl.name] = formattedOption === 'Select a Country'.toLowerCase() ? '' : formattedOption;
-                    return accum;
-                }
-                    
-                // TODO: This may not be the most robust way to get form data
-                case 'DIV': {
-                    const checkedRadioEl = preTestEl.querySelector('input[type="radio"]:checked');
-                  
-                    if (checkedRadioEl) accum[checkedRadioEl.name] = checkedRadioEl.dataset.radioOption === 'yes';
-                    return accum;
-                }
-            }
+					accum[preTestEl.name] = inputValue;
+					return accum;
+				}
 
-            return accum;
-        }, {});
+				case 'SELECT': {
+					const formattedOption = preTestEl.options[preTestEl.selectedIndex].text.trim().toLowerCase();
 
-        const isValidUserInfo = (userInfo = {}) => {
-            if (!userInfo || Object.keys(userInfo).length ===0) return false;
+					accum[preTestEl.name] = formattedOption === 'Select a Country'.toLowerCase() ? '' : formattedOption;
+					return accum;
+				}
+					
+				// TODO: This may not be the most robust way to get form data
+				case 'DIV': {
+					const checkedRadioEl = preTestEl.querySelector('input[type="radio"]:checked');
+				  
+					if (checkedRadioEl) accum[checkedRadioEl.name] = checkedRadioEl.dataset.radioOption === 'yes';
+					return accum;
+				}
+			}
 
-            for (const prop in userInfo) {
-                // TODO: No ... just... no.
-                if (typeof userInfo[prop] === 'undefined' || userInfo[prop] === null || userInfo[prop] === '') return false;
-            }
+			return accum;
+		}, {});
 
-            return true;
-        }
+		const isValidUserInfo = (userInfo = {}) => {
+			if (!userInfo || Object.keys(userInfo).length ===0) return false;
 
-        if (isValidUserInfo(userInfo)) {
-            postPreArrivalTest(userInfo)
-            .then(response => {
-                if (response.status >= 400) throw Error(response.statusText);
+			for (const prop in userInfo) {
+				// TODO: No ... just... no.
+				if (typeof userInfo[prop] === 'undefined' || userInfo[prop] === null || userInfo[prop] === '') return false;
+			}
 
-                // TODO: Make a generic function for this rendering behavior
-                mainContentEl.innerHTML = '';
-                mainContentEl.appendChild(proficiencyTestNode);
+			return true;
+		}
 
-                return;
-            })
-            .catch(err => {
-                // TODO: Handle errors from the server ... eventually
-                console.log('error with test', err)
-            });
+		if (isValidUserInfo(userInfo)) {
+			postPreArrivalTest(userInfo)
+			.then(response => {
+				if (response.status >= 400) throw Error(response.statusText);
 
-        } else {
-            console.log('user info is not valid');
-            // TODO: Error handling
-        }
-    }
+				// TODO: Make a generic function for this rendering behavior
+				mainContentEl.innerHTML = '';
+				mainContentEl.appendChild(proficiencyTestNode);
 
-    if (clickedEl.classList.contains('pt__submit-grade-button')) {    
-        // Convert the nodeList querySelectorAll returns into an array
-        const testQuestionsEls = Array.apply(null, document.querySelectorAll('.pt__test-question-container')),
-            testGlobals = document.querySelector('.pt__test-globals').dataset;
+				return;
+			})
+			.catch(err => {
+				// TODO: Handle errors from the server ... eventually
+				console.log('error with test', err)
+			});
 
-        let testResponseData = [];
+		} else {
+			document.querySelector('.pt__error-container').classList.remove('is-hidden');
+			console.log('user info is not valid');
+			// TODO: Error handling
+		}
+	}
 
-        testQuestionsEls.forEach(testQuestionEl => {
-            const testQuestionOptions = Array.apply(null, testQuestionEl.querySelectorAll('.pt__test-options-container input')),
-                selectedOption = testQuestionOptions.find(testQuestionOption => testQuestionOption.checked);
+	if (clickedEl.classList.contains('pt__submit-grade-button')) {    
+		// Convert the nodeList querySelectorAll returns into an array
+		const testQuestionsEls = Array.apply(null, document.querySelectorAll('.pt__test-question-container')),
+			testGlobals = document.querySelector('.pt__test-globals').dataset;
 
-            if (selectedOption)  {
-                testResponseData.push({
-                    questionId: testQuestionEl.dataset.questionId,
-                    selectedOptionId: selectedOption.dataset.optionId
-                })
-            } 
-        });
+		let testResponseData = [];
 
-        // TODO: The function definitions should be pulled out of here, maybe put in another file
-        const postGradeTest = async (testResponses) => {
-            const response = await fetch('/grade-test', { 
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(testResponses)
-                });
+		testQuestionsEls.forEach(testQuestionEl => {
+			const testQuestionOptions = Array.apply(null, testQuestionEl.querySelectorAll('.pt__test-options-container input')),
+				selectedOption = testQuestionOptions.find(testQuestionOption => testQuestionOption.checked);
 
-            return response;
-        }
+			if (selectedOption)  {
+				testResponseData.push({
+					questionId: testQuestionEl.dataset.questionId,
+					selectedOptionId: selectedOption.dataset.optionId
+				})
+			} 
+		});
 
-        console.log('questions?', testGlobals.numTestQuestions);
-        if (testResponseData.length < testGlobals.numTestQuestions) {
-            // TODO: Error handling
-            console.log(`user didn't answer all qu1estions`);
-        } else {
-            postGradeTest(testResponseData)
-            .then(response => {
-                console.log('we posted!');
-                if (response.status >= 400) throw Error(response.statusText);
+		// TODO: The function definitions should be pulled out of here, maybe put in another file
+		const postGradeTest = async (testResponses) => {
+			const response = await fetch('/grade-test', { 
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(testResponses)
+				});
 
-                // TODO: This doesn't feel particularly elegant
-                mainContentEl.innerHTML = '';
-                mainContentEl.append(completedNode);
+			return response;
+		}
 
-                return response.json();
-            })
-            .then(parsedResponse => {
-                console.log('the parsed response!', parsedResponse);
+		if (testResponseData.length < testGlobals.numTestQuestions) {
+			// TODO: Error handling
+			document.querySelector('.pt__error-container').classList.remove('is-hidden');
+		} else {
+			postGradeTest(testResponseData)
+			.then(response => {
+				if (response.status >= 400) throw Error(response.statusText);
 
-                return;
-            })
-            .catch(err => {
-                // TODO: Handle errors from the server ... eventually
-                console.log('error with test', err)
-            });
-        }
-    }
+				// TODO: This doesn't feel particularly elegant
+				mainContentEl.innerHTML = '';
+				mainContentEl.append(completedNode);
+
+				return response.json();
+			})
+			.then(parsedResponse => {
+				console.log('the parsed response!', parsedResponse);
+
+				return;
+			})
+			.catch(err => {
+				// TODO: Handle errors from the server ... eventually
+				console.log('error with test', err)
+			});
+		}
+	}
 });
 
 // Navbar color change logic
 const navbarEl = document.querySelector('.navbar'),
-    colorChangePoint = navbarEl.offsetTop;
+	colorChangePoint = navbarEl.offsetTop;
 
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > colorChangePoint) {
-        navbarEl.classList.add('navbar--scrolled');
-    } else {
-        navbarEl.classList.remove('navbar--scrolled');
-    }
+	if (window.pageYOffset > colorChangePoint) {
+		navbarEl.classList.add('navbar--scrolled');
+	} else {
+		navbarEl.classList.remove('navbar--scrolled');
+	}
 })
--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,16 @@
-const functions = require('firebase-functions'),
-    express = require('express'),
-    path = require('path'),
-    jsonfile = require('jsonfile'),
-    bodyParser = require('body-parser'),
-    nodemailer = require('nodemailer'),
+const express = require('express'),
+	path = require('path'),
+	jsonfile = require('jsonfile'),
+	bodyParser = require('body-parser'),
+	nodemailer = require('nodemailer'),
+	smtpTransport = require('nodemailer-smtp-transport'),
 // TODO: Figure out if there's a way to only get the 'auto' function
-    async = require('async'),
-    engines = require('consolidate');
+	async = require('async'),
+	engines = require('consolidate');
+
+// Bring in env variables from Firebase
+const functions = require('firebase-functions');
+const e = require('express');
 
 const app = express();
 
@@ -24,146 +28,188 @@ app.use(express.static(path.join(__dirname, 'assets')));
 // Variables to hold on to very simple state
 // TODO: May need to rethink this, eventually
 let globals = {
-    numTestQuestions: null,
-    userData: null
+	numTestQuestions: null,
+	userData: null
 }
 
 app.get('/', (req, res) => {
-    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
 
-    return async.auto({
-        countriesJson: cb => {
-            jsonfile.readFile(path.join(__dirname, 'assets/js/countries.json'), (err, obj) => { // eslint-disable-line
-                if (err) {
-                    console.log('error retrieving countries json', err);
-                    return cb(err);
-                }
-        
-                cb(null, obj);
-            });
-        },
-        testJson: cb => {
-            jsonfile.readFile(path.join(__dirname, 'assets/js/proficiency-test.json'), (err, obj) => { // eslint-disable-line
-                if (err) {
-                    console.log('error retrieving proficiency-test json', err);
-                    return cb(err);
-                }
-        
-                cb(null, obj);
-            });
-        }
-    }, (err, results) => {
-        if (err) console.log('something went wrong reading the JSONs', err);
+	return async.auto({
+		countriesJson: cb => {
+			jsonfile.readFile(path.join(__dirname, 'assets/js/countries.json'), (err, obj) => { // eslint-disable-line
+				if (err) {
+					console.log('error retrieving countries json', err);
+					return cb(err);
+				}
+		
+				cb(null, obj);
+			});
+		},
+		testJson: cb => {
+			jsonfile.readFile(path.join(__dirname, 'assets/js/proficiency-test.json'), (err, obj) => { // eslint-disable-line
+				if (err) {
+					console.log('error retrieving proficiency-test json', err);
+					return cb(err);
+				}
+		
+				cb(null, obj);
+			});
+		}
+	}, (err, results) => {
+		if (err) console.log('something went wrong reading the JSONs', err);
 
-        globals.numTestQuestions = results.testJson.length;
+		globals.numTestQuestions = results.testJson.length;
 
-        const pageProperties = {
-            title: 'FLS International Proficiency Test',
-            countries: results.countriesJson || null,
-            ptQuestions: results.testJson || null,
-            numTestQuestions: globals.numTestQuestions,
-            // TODO: Investigate a better method for importing partials
-            partials: {
-                preArrivalTest: path.join(__dirname, 'assets/partials/preArrivalTest'),
-                proficiencyTest: path.join(__dirname, 'assets/partials/proficiencyTest'),
-                completed: path.join(__dirname, 'assets/partials/completed'),
-                loader: path.join(__dirname, 'assets/partials/loader'),
-                scripts: path.join(__dirname, 'assets/partials/scripts')
-            }
-        };
-    
-        return res.render('index', pageProperties)
-    });
+		const pageProperties = {
+			title: 'FLS International Proficiency Test',
+			countries: results.countriesJson || null,
+			ptQuestions: results.testJson || null,
+			numTestQuestions: globals.numTestQuestions,
+			// TODO: Investigate a better method for importing partials
+			partials: {
+				preArrivalTest: path.join(__dirname, 'assets/partials/preArrivalTest'),
+				proficiencyTest: path.join(__dirname, 'assets/partials/proficiencyTest'),
+				completed: path.join(__dirname, 'assets/partials/completed'),
+				loader: path.join(__dirname, 'assets/partials/loader'),
+				scripts: path.join(__dirname, 'assets/partials/scripts')
+			}
+		};
+	
+		return res.render('index', pageProperties)
+	});
 });
 
 app.post('/pre-arrival-test', (req, res) => {
-    // TODO: Needs some server side validation
-    globals.userData = req.body;
+	// TODO: Needs some server side validation
+	globals.userData = req.body;
 
-    res.status(200).json({ msg: 'successfully submitted pre-arrival test responses', payload: req.body });
+	res.status(200).json({ msg: 'successfully submitted pre-arrival test responses', payload: req.body });
 });
 
 app.post('/grade-test', (req, res) => {
-    // TODO: Implement caching for JSON files
-    return jsonfile.readFile(path.join(__dirname, 'assets/js/proficiency-test-answers.json'), (err, testAnswers) => {
-        if (err) {
-            console.log('error retrieving proficiency-test-answers json', err);
-            return res.status(400).send( { err });
-        }
+	// TODO: Implement caching for JSON files
+	return jsonfile.readFile(path.join(__dirname, 'assets/js/proficiency-test-answers.json'), (err, testAnswers) => {
+		if (err) {
+			console.log('error retrieving proficiency-test-answers json', err);
+			return res.status(400).send( { err });
+		}
 
-        const testResponseData = req.body;
+		const testResponseData = req.body;
 
-        console.log('testResponeData length & type', testResponseData.length, typeof testResponseData);
-        console.log('globals.numTestQuestions length & type', globals.numTestQuestions.length, typeof globals.numTestQuestions);
-        if (testResponseData.length !== globals.numTestQuestions) return res.status(400).send({ err: 'Please answer all test questions' });
-    
-        // TODO: Should really include logic that ensures the correct test questions are being compared to the correct answers
-        const gradeTest = (testResponseData, testAnswers) => {
-            let numCorrect = 0;
+		console.log('testResponeData length & type', testResponseData.length, typeof testResponseData);
+		console.log('globals.numTestQuestions length & type', globals.numTestQuestions, typeof globals.numTestQuestions);
+		if (testResponseData.length !== globals.numTestQuestions) return res.status(400).send({ err: 'Please answer all test questions' });
+	
+		// TODO: Should really include logic that ensures the correct test questions are being compared to the correct answers
+		const gradeTest = (testResponseData, testAnswers) => {
+			let numCorrect = 0;
 
-            for (let i = 0; i < testResponseData.length; i++) {
-                if (testResponseData[i].selectedOptionId == testAnswers[i].correctOptionId) numCorrect++; // eslint-disable-line
-            }
+			for (let i = 0; i < testResponseData.length; i++) {
+				if (testResponseData[i].selectedOptionId == testAnswers[i].correctOptionId) numCorrect++; // eslint-disable-line
+			}
 
-            return numCorrect;
-        }
+			return numCorrect;
+		}
 
-        const generateHtml = (userData) => {
-            let html = '<h2>Pre Arrival User Info</h2>';
+		const generateHtml = (userData) => {
+			let html = '<h2>Pre Arrival User Info</h2>';
 
-            for (const prop in userData) {
-                html += `<p><strong>${prop}: </strong>${userData[prop]}</p>`;
-            }
+			for (const prop in userData) {
+				html += `<p><strong>${prop}: </strong>${userData[prop]}</p>`;
+			}
 
-            return html;
-        };
-        // TODO: This very obviously needs to be extracted, likely into another file
-        // TODO: This also needs to send an email to the email provided in the pre arrival test
-        const emailTestResults = async () => {
-            console.log('constructing email', process.env.EMAIL_PASSWORD);
-            const transporter = nodemailer.createTransport({
-                host: 'smtp.gmail.com',
-                port: 587,
-                // TODO: Does this need to be true?
-                secure: false,
-                auth: {
-                    user: process.env.EMAIL_USERNAME,
-                    pass: process.env.EMAIL_PASSWORD
-                }
-            });
+			return html;
+		};
+		// TODO: This very obviously needs to be extracted, likely into another file
+		// TODO: This also needs to send an email to the email provided in the pre arrival test
+		const emailTestResults = async () => {
+			const transporter = nodemailer.createTransport(smtpTransport({
+				// TODO: Host & Post should also be ENV variables
+				name: 'hostgator',
+				host: 'gator3209.hostgator.com',
+				port: 465,
+				// TODO: Does this need to be true?
+				secure: true,
+				auth: {
+					user: functions.config().email.username,
+					pass: functions.config().email.password
+				}
+			}));
 
-            // TODO: You're getting tired, Gabriel.
-            const generateHtml = (userData) => {
-                let html = '<h2>Pre Arrival User Info</h2>';
+			// TODO: You're getting tired, Gabriel.
+			const generateHtml = (userData) => {
+				let html = '<h2>Pre Arrival User Info</h2>';
 
-                for (const prop in userData) {
-                    html += `<p><strong>${prop}: </strong>${userData[prop]}</p>`;
-                }
+				for (const prop in userData) {
+					html += `<p><strong>${prop}: </strong>${userData[prop]}</p>`;
+				}
 
-                return html;
-            };
+				return html;
+			};
 
-            const info = await transporter.sendMail({
-                from: 'gabriel gonzalvez',
-                to: 'akingdebased@gmail.com',
-                subject: 'Test Results',
-                // TODO: This, rather obviously, needs finessing
-                html: `${generateHtml(globals.userData)}<h2>Proficiency Test Results</h2><p>Correct responses: <strong>${gradeTest(testResponseData, testAnswers)}/${globals.numTestQuestions}</strong></p>`
-            });
+			const testResults = gradeTest(testResponseData, testAnswers);
 
-            console.log(`email has been sent with messageID ${info.messageId}`);
-        }
+			let level;
+			let rank;
 
-        return emailTestResults()
-        .then(() => {
-            // TODO: Likely needs more in depth handling here
-            return res.status(200).json({ msg: 'successfully graded test', gradedTest: gradeTest(testResponseData, testAnswers) });
-        })
-        .catch(err => {
-            console.log('error sending email', err)
-            return res.status(400).json({ err: err});
-        });
-    });
+			// TODO: Ensure this is done elegantly
+			if (testResults >= 0 && testResults <= 7) {
+				level = '1-2';
+				rank = 'beginner';
+			} else if (testResults >= 8 && testResults <= 14) {
+				level = '3-4';
+				rank = 'beginner';
+			} else if (testResults >= 15 && testResults <= 21) {
+				level = '5-6';
+				rank = 'beginner';
+			} else if (testResults >= 22 && testResults <= 27) {
+				level = '7-8';
+				rank = 'intermediate';
+			} else if (testResults >= 28 && testResults <= 33) {
+				level = '9-10';
+				rank = 'intermediate';
+			} else if (testResults >= 33 && testResults <= 40) {
+				// TODO: Is it intentional that this 33 overlaps the previous rank?
+				level = '11 - 12';
+				rank = 'intermediate';
+			} else if (testResults >= 41 && testResults <= 45) {
+				level = '13-14';
+				rank = 'advanced';
+			} else if (testResults >= 46 && testResults <= 50) {
+				level = '15-16';
+				rank = 'advanced';
+			}
+
+			const emailHtml = `${generateHtml(globals.userData)}<h2>Proficiency Test Results</h2><p><strong>Correct responses:</strong> ${testResults}/${globals.numTestQuestions}</p><p><strong>Level:</strong> ${level}</p><p><strong>Proficiency level:</strong> ${rank}</p>`;
+
+			console.log(`email html: ${emailHtml}`)
+
+			return transporter.sendMail({
+				// TODO: Host & port should also be env variables
+				from: 'FLS International',
+				to: 'akingdebased@gmail.com',
+				subject: 'Test Results',
+				// TODO: This, rather obviously, needs finessing
+				html: emailHtml
+			}, (error, info) => {
+				console.log('email error', error);
+				console.log('email okay?', info);
+			});
+		}
+
+		return emailTestResults()
+		.then(emailResults => {
+			console.log('email Results?', emailResults);
+			// console.log(`email has been sent with messageID ${emailResults.messageId}`);
+			// TODO: Likely needs more in depth handling here
+			return res.status(200).json({ msg: 'successfully graded test', gradedTest: gradeTest(testResponseData, testAnswers) });
+		})
+		.catch(err => {
+			console.log('error sending email', err)
+			return res.status(400).json({ err: err});
+		});
+	});
 });
 
 exports.app = functions.https.onRequest(app);
